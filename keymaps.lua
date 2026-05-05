b732994c1c314f6ed87632263a4fa756237f07fe
nunya <kerbalcadet@gmail.com>
2026-05-05 05:09:01 -0500

    init

---
+++ keymaps.lua
@@ -0,0 +1,15 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("t", "<C-n>", "<C-\\><C-n>")
+vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>")
+vim.keymap.set("n", "<M-w>", function()
+  Snacks.bufdelete()
+end)
+vim.keymap.set("n", "<M-q>", "ZZ")
+vim.keymap.set("n", "<leader>q", "ZZ", { remap = true })
+vim.keymap.set("n", "sb", "<cmd>set scb<cr>")
+vim.keymap.set({ "n", "x" }, "<M-f>", ":%s/<C-r>+//g<left><left>")
+vim.keymap.set({ "n", "x" }, "<M-F>", "0k/<C-r>+<cr>")
+vim.keymap.set("n", "<F2>", vim.lsp.buf.rename)
