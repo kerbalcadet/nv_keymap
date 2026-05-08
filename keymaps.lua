06b1876107bd952be117bc55481ee3cac3aec2cd
Nick <nick.woodruff@knightly.tech>
2026-05-08 16:25:31 -0500

    auto

--- keymaps.lua
+++ keymaps.lua
@@ -39,4 +39,4 @@ vim.keymap.set("x", "<M-L>", "ygvxEp/<C-r>+<cr>vgn")
 
 -- search / ctrl+d ish
 vim.keymap.set("x", "<M-/>", "yh/<C-r>+<cr>`><esc>")
-vim.keymap.set("n", "<M-n>", "lgn<esc>")
+vim.keymap.set("n", "<M-n>", "$")
