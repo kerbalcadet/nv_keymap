55b07622cbf974a0905abf8256695514a1cb31fa
Nick <nick.woodruff@knightly.tech>
2026-05-09 00:19:49 -0500

    auto

--- keymaps.lua
+++ keymaps.lua
@@ -59,11 +59,12 @@ end
 local function start_search_offset(search_text_)
   search_text = search_text_ or vim.fn.expand("<cword>")
 
-  local c_col = vim.api.nvim_win_get_cursor(0)[2]
+  local c_row, c_col = unpack(vim.api.nvim_win_get_cursor(0))
   local p_col = vim.fn.searchpos(search_text, "bcW")[2]
   offset = c_col - p_col
 
-  --  vim.cmd.normal({ args = { "<esc>", (offset + 1) .. "l" } })
+  vim.api.nvim_win_set_cursor(0, { c_row, c_col })
+  vim.cmd("normal <esc>")
   vim.fn.setreg("/", search_text)
   vim.cmd("set hlsearch")
 end
