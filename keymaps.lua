21158900ba0c9556c18db808462eeda24e223767
Nick <nick.woodruff@knightly.tech>
2026-05-08 21:05:19 -0500

    auto

--- keymaps.lua
+++ keymaps.lua
@@ -44,8 +44,8 @@ local function start_search_offset()
   search_text = vim.fn.getreg("s")
   local c_col = vim.api.nvim_win_get_cursor(0)[2]
   local p_col = vim.fn.searchpos(search_text, "cn")[2]
-  --offset = c_col - p_col
-  --vim.cmd("normal! <esc>")
+  offset = c_col - p_col
+  vim.cmd("normal! <esc>")
 end
 
 local function next_search_offset()
